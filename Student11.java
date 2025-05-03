1cea95fb9131a4d0de8ec76926aa70b29b337dbd
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-05-03 10:23:28 +0530

    done

---
+++ Student11.java
@@ -0,0 +1,28 @@
+//Create a class named 'Student11' with String variable 'name' and integer 
+variable 'roll_no'. Assign the value of roll_no as '2' and that of name as 
+"John" by creating an object of the class Student.
+
+ class Student11 {
+       String name;
+       int rollno;
+ public static void main (String args[]){
+    Student11 s=new Student11();
+     s.name="john";
+     s.rollno=2;
+System.out.println("name of student11 is ="+s.name);
+System.out.println("roll no of student11 is ="+s.rollno);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
