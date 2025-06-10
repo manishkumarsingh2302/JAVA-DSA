812589a94946ccf74bfcbcd2cc347b2ea6980edc
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-06-11 00:03:36 +0530

    done

---
+++ Animal .java
@@ -0,0 +1,11 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal sound");
+    }
+}
+
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
