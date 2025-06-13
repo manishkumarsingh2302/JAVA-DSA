f413e937dc2006862ad4d848bdbdc7de81d18ae5
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-06-13 11:27:51 +0530

    done

---
+++ ToCharArray.java
@@ -0,0 +1,9 @@
+public class ToCharArray {
+    public static void main(String[] args) {
+        String s = "JAVA";
+        char[] ch = s.toCharArray();
+        for (char c : ch) {
+            System.out.println(c);
+        }
+    }
+}
