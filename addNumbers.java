e34512c2dba6d33b9f58481f75acae2bda049ee2
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-06-20 13:48:42 +0530

    done

---
+++ addNumbers.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    // This is a function to add two numbers
+    public static int addNumbers(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int result = addNumbers(5, 10);
+        System.out.println("The sum is: " + result);
+    }
+}
