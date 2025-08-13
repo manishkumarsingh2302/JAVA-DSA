2cef4bb99aae0fab7b640a4b00c6bcc471e18007
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-08-13 13:55:58 +0530

    done

---
+++ Main12.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] problems = new int[4];
+        for (int i = 0; i < 4; i++) {
+            problems[i] = sc.nextInt();
+        }
+        
+        int target = 10;
+        int count = 0;
+        for (int i = 0; i < 4; i++) {
+            if (problems[i] >= target) {
+                count++;
+            }
+        }
+        
+        System.out.println(count);
+        sc.close();
+    }
+}
