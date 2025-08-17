06cf42bb91ee5cf046a6929240894d9bbbb63b3a
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-08-17 14:08:08 +0530

    done

---
+++ Main3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = "manish";
+        // String gtr = "mahi";
+        // System.out.println(str.compareTo(gtr));
+        System.out.println(str.contains("bnm"));
+    }
+}
--- main2.java
+++ main2.java
@@ -1,30 +1,14 @@
-//serch 16 in these given box [1,2,3,4,5,6,7,8,9,10] in binary serch?
-
-class main2{
+import java.util.*;
+ class main2 {
     public static void main(String[] args) {
-        int a []={1,2,3,4,5,6,7,8,9,10};
-        int strt=0;
-        int end=a.length-1;
-        int mid=(strt+end)/2;
-        int serch=8;
-        while (strt<=end) {
-            if(a[mid]==serch){
-                System.out.println("our serch value id index ="+mid);
-                break;
-            } 
+        Scanner sc=new Scanner(System.in);
+        String str="manish";
+        String gtr="mahi";
+        System.out.println(str.compareTo(gtr));
 
-            else if(mid<serch){
-                strt=mid+1;
-            }
-            else{
-                end=mid-1;
-            }
-            mid=(strt+end)/2;
-            
-        }
-    
-        if(strt>end){
-         System.out.println("your serch number not in this line");
-        }
     }
+
+    
 }
+
+
