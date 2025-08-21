937f61895ded4a0c72db7a3de4a3c2c4cdb3193e
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-08-21 13:08:10 +0530

    done

--- Main3.java
+++ Main3.java
@@ -1,11 +1,10 @@
+
 public class Main3 {
     public static void main(String[] args) {
-        int a[]={1,2,3,4,5,6};
-        int sum=0;
-        for(int i=0; i<a.length;i++){
-            sum+=a[i];
-        }
-        System.out.println("sum is:"+sum);
 
+        int no=4568;
+        
+        int a[]={2,3,4,5,6,7};
+        
     }
 }
